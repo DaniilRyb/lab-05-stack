ce65e7b4a8a229af634f6132522b5ace17ff8510
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2020-12-22 20:37:21 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,8 @@
 // Copyright 2020 Daniil Rybakov <pochtaworld7@gmail.com>
 
 #include <gtest/gtest.h>
-
+#include <Stack.hpp>
+#include <stack2.hpp>
 TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
 }
